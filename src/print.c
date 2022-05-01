b3f25b3fd524d7fe244f2369b5685166b0431816
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-05-01 15:26:42 -0300

    first trash working program

--- src/print.c
+++ src/print.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 enum TYPES{
-   SIGNED_INT, UNSIGNED_INT, LONG, LONG_LONG, DOUBLE, FLOAT
+   SIGNED_INT, UNSIGNED_INT, LONG, LONG_LONG, DOUBLE, FLOAT, STRING, LONG_UNSIGNED_INT
 };
 
 typedef struct t_arg_types{
@@ -22,7 +22,11 @@ void insert_in_type_list (char);
 void convert_arguments_to_string (va_list, char *[]);
 
 int main(void) {
-    print("Olá bebe %d\n", 10 + 20);
+
+    int x = 10;
+    size_t y = 124124123;
+
+    print("Unsigned long integer variable: %t", y);
     return 0;    
 
 }
@@ -33,7 +37,7 @@ int print(char *string, ...) {
     int length_without_place_holders = length;
     int num_of_args = 0;
     char place_holders[] = {
-        'd', 'f', 'u', 's'
+        'd', 'f', 'u', 's', 't'
     };
     
     va_list arguments;
@@ -66,34 +70,29 @@ int print(char *string, ...) {
     char *converted_str[num_of_args];
     convert_arguments_to_string(arguments, converted_str);
     
-   int string_final_length = length_without_place_holders;
+    int string_final_length = length_without_place_holders ;
 
-   for (int i = 0; i < num_of_args; ++i) {
-        string_final_length += strlen(converted_str[i]);
-    }
-    char *str = (char *) malloc(string_final_length);
-    strcpy (str, string);
+    for (int i = 0; i < num_of_args; ++i) string_final_length += strlen(converted_str[i]); 
+    char *final_string = (char*) malloc(string_final_length);
 
-    for (int i = 0, j = 0; i < string_final_length; ++i) {
-        if (str[i] == '%') {
-            for (int z = 0; z < strlen(converted_str[j]); ++z) {
-              if (i + z != i || i + z != i + 1) {
-                str[i+z+1] = str[i+z];
-                str[i+z] = converted_str[j][z];
-              } 
+    for (int i = 0, final_string_id = 0, arg_index = 0; i < string_final_length; ++i, ++final_string_id) {
+        if (string[i] == '%') {
+            i += 2;
+            for (int z = 0; z < strlen(converted_str[arg_index]); ++z) {
+                final_string[final_string_id] = converted_str[arg_index][z];
+                final_string_id += 1;
+            
             }
-            j += 1;
-        }
-    } 
-
-
+            arg_index += 1;
+        } 
+        final_string[final_string_id] = string[i];
+    }
+   
     va_end(arguments);
     register int syscall_no asm("rax") = 1;
     register int arg1 asm("rdi") = 1;
-    register char* arg2 asm("rsi") = str;
-    register int arg3 asm("rdx") = string_final_length ;
-    asm("syscall");
-    return 0;
+    register char* arg2 asm("rsi") = final_string;
+    register int arg3 asm("rdx") = string_final_length ; asm("syscall"); return 0;
 }
 
 void insert_in_type_list(char place_holder) {
@@ -102,7 +101,8 @@ void insert_in_type_list(char place_holder) {
     if (place_holder == 'd') new_type->type = SIGNED_INT;
     else if (place_holder == 'f') new_type->type = FLOAT;
     else if (place_holder == 'u') new_type->type = UNSIGNED_INT;
-
+    else if (place_holder == 's') new_type->type = STRING;
+    else if (place_holder == 't') new_type->type = LONG_UNSIGNED_INT;
 
     if (arg_types_head == NULL) {
         arg_types_head = new_type;
@@ -130,11 +130,22 @@ void convert_arguments_to_string (va_list arguments, char *buffer[]) {
         } 
         else if (arg_types_head->type == UNSIGNED_INT) {
             
-            int arg = va_arg(arguments, unsigned int );
+            unsigned int arg = va_arg(arguments, unsigned int );
             int length = snprintf(NULL, 0, "%u", arg);
             buffer[i] = (char *) malloc(length + 1);
             snprintf(buffer[i], length + 1, "%u", arg );
         }
+        else if (arg_types_head->type == STRING) {
+            buffer[i] = va_arg(arguments, char*);
+
+        }
+        else if (arg_types_head->type == LONG_UNSIGNED_INT) {
+            size_t arg = va_arg(arguments, size_t);
+            int length = snprintf(NULL, 0, "%lu", arg);
+            buffer[i] = (char*) malloc(length + 1);
+            snprintf(buffer[i], length + 1, "%lu", arg);
+        }
+        i += 1;
         arg_types_head = arg_types_head->next_arg;
     }
 }
